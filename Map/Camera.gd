1733a3ce2a75ad9a34f5b42fbe2d78ca017c42a8
Ciaran Woodward <ciaranwoodward@gmail.com>
2021-10-02 19:15:09 +0100

    Finish up camera control for now

--- Map/Camera.gd
+++ Map/Camera.gd
@@ -1,11 +1,15 @@
 extends Camera
 
-export var MAX_HEIGHT : float = 95.0
-export var MIN_HEIGHT : float = 20.0
+export var MAX_HEIGHT : float = 125.0
+export var MIN_HEIGHT : float = 10.0
 export var distance : float = 120.0
 export var MAX_SPEED = 10.0
 export var ACCEL_TIME = 0.5
 export var STOP_TIME = 0.2
+export var ZOOM_AMOUNT = 0.5
+export var ZOOM_TIME = 0.2
+export var ZOOM_MIN = 1.0
+export var ZOOM_MAX = 4.0
 
 onready var tween = get_node("Tween")
 
@@ -14,12 +18,75 @@ var dir_vec = Vector2.ZERO
 var speed = 0
 var height = 35.0
 var moving = false
+var dragging = false
+var rezoom = false
+var zoom = 1.0
+var target_zoom = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _unhandled_input(event):
+	# Only allow unhandled input for start drag
+	if event is InputEventMouseButton and (event.button_index == BUTTON_RIGHT || event.button_index == BUTTON_MIDDLE):
+		if event.is_pressed():
+			dragging = true
+			tween.remove(self, "dir_vec")
+			tween.remove(self, "speed")
+			speed = 0
+			moving = false
+			dir_vec = Vector2.ZERO
+	if event.is_action_pressed("zoom_in"):
+		target_zoom += ZOOM_AMOUNT
+		#rezoom = true # Disabled zooming, it felt a bit janky
+	if event.is_action_pressed("zoom_out"):
+		target_zoom -= ZOOM_AMOUNT
+		#rezoom = true
+
+func _input(event):
+	# Once we have started dragging, always track the input
+	if not dragging:
+		return
+	if event is InputEventMouseButton and (event.button_index == BUTTON_RIGHT || event.button_index == BUTTON_MIDDLE):
+		if not event.is_pressed():
+			dragging = false
+	if dragging && event is InputEventMouseMotion:
+		angle += event.relative.x / (250.0 * zoom)
+		height += event.relative.y / 2.0
+		_bound_level()
+
+func _bound_level():
+	while (angle < 0):
+		angle += PI * 2
+	while (angle > PI * 2):
+		angle -= PI * 2
+	if height < MIN_HEIGHT:
+		height = MIN_HEIGHT
+	if height > MAX_HEIGHT:
+		height = MAX_HEIGHT
+	
+	var camdist = distance - (distance * (zoom - ZOOM_MIN) / (ZOOM_MAX * 2))
+	self.translation = Vector3(cos(angle) * camdist, height, sin(angle) * camdist)
+	var lookatpoint = Vector3(cos(angle) * distance * (zoom - ZOOM_MIN) / (ZOOM_MAX * 2), 0, sin(angle) * distance * (zoom - ZOOM_MIN) / (ZOOM_MAX * 2))
+	self.look_at(lookatpoint, Vector3(0, 1, 0))
+
 func _process(delta: float) -> void:
+	if rezoom:
+		if target_zoom > ZOOM_MAX:
+			target_zoom = ZOOM_MAX
+		if target_zoom < ZOOM_MIN:
+			target_zoom = ZOOM_MIN
+		if target_zoom != zoom:
+			tween.remove(self, "zoom")
+			tween.interpolate_property(self, "zoom", zoom, target_zoom, ZOOM_TIME, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+			tween.start()
+		rezoom = false
+	
+	if dragging:
+		_bound_level()
+		return
+	
 	var dir_delta = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		dir_delta.x += 1
@@ -54,15 +121,4 @@ func _process(delta: float) -> void:
 	angle += dir_vec.x * speed * delta
 	height += dir_vec.y * speed * 20.0 * delta
 	
-	while (angle < 0):
-		angle += PI * 2
-	while (angle > PI * 2):
-		angle -= PI * 2
-	
-	if height < MIN_HEIGHT:
-		height = MIN_HEIGHT
-	if height > MAX_HEIGHT:
-		height = MAX_HEIGHT
-	
-	self.translation = Vector3(cos(angle) * distance, height, sin(angle) * distance)
-	self.look_at(Vector3(0,0,0), Vector3(0, 1, 0))
+	_bound_level()
